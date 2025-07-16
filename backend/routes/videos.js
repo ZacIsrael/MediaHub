95281ad9b0f11aae691019ff1ff99cc5748e1b9f
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-07-16 17:27:12 -0400

    Implemented request error handling in the POST route for adding videos.

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -16,30 +16,88 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   // Thoroughly check the body of the request for errors (will create a
   // an auxillary function in helpers.js later on)
+  if (
+    req.body.hasOwnProperty("title") &&
+    req.body.hasOwnProperty("url") &&
+    req.body.hasOwnProperty("viewCount") &&
+    req.body.hasOwnProperty("publishedAt")
+  ) {
+    // extract the necessary fields from the body of the request
+    // tags will be set to an empty array if it's not in the body of the request (it's an optional field)
+    const { title, url, tags = [], viewCount, publishedAt } = req.body;
+    console.log("tags = ", tags);
+    if (typeof title !== "string") {
+      // For some reason, title is not of type string; throw error
+      res.status(400).json({
+        error: `Error (\'/api/videos/\' POST route): \'title\'field must be of type \'string\'.`,
+      });
+    }
 
-  // happy path (for now)
-  // extract the necessary fields from the body of the request
-  const { title, url, tags, viewCount, publishedAt } = req.body;
+    if (typeof url !== "string") {
+      // For some reason, url is not of type string; throw error
+      res.status(400).json({
+        error: `Error (\'/api/videos/\' POST route): \'url\'field must be of type \'string\'.`,
+      });
+    }
 
-  try {
-    // insert the new video into the mongoDB collection
-    const newVideo = await Videos.create({
-      title,
-      url,
-      tags,
-      viewCount,
-      publishedAt,
-    });
+    if (typeof publishedAt !== "string") {
+      // For some reason, publishedAt is not of type string; throw error
+      res.status(400).json({
+        error: `Error (\'/api/videos/\' POST route): \'publishedAt\'field must be of type \'string\'.`,
+      });
+    }
 
-    res.status(201).json({
-      message: `Successfully inserted video into the \'videos\' mongoDB collection.`,
-      video: newVideo,
-    });
-  } catch (err) {
-    // Error inserting the video into the mongoDB collection
-    res.status(500).json({
-      error: `Error (\'/api/videos/\' POST route): ${err.message}`,
-      stack: err.stack,
+    if (typeof viewCount !== "number" || isNaN(viewCount)) {
+      // For some reason, viewCount is not of type number; throw error
+      res.status(400).json({
+        error: `Error (\'/api/videos/\' POST route): \'viewCount\'field must be of type \'number\'.`,
+      });
+    }
+
+    // check for empty strings
+    if (title.trim().length === 0) {
+      res.status(400).json({
+        error: `Error (\'/api/videos/\' POST route): \'title\' field is an empty string.`,
+      });
+    }
+
+    if (url.trim().length === 0) {
+      res.status(400).json({
+        error: `Error (\'/api/videos/\' POST route): \'url\' field is an empty string.`,
+      });
+    }
+
+    if (publishedAt.trim().length === 0) {
+      res.status(400).json({
+        error: `Error (\'/api/videos/\' POST route): \'publishedAt\' field is an empty string.`,
+      });
+    }
+
+    try {
+      // insert the new video into the mongoDB collection
+      const newVideo = await Videos.create({
+        title,
+        url,
+        tags,
+        viewCount,
+        publishedAt,
+      });
+
+      res.status(201).json({
+        message: `Successfully inserted video into the \'videos\' mongoDB collection.`,
+        video: newVideo,
+      });
+    } catch (err) {
+      // Error inserting the video into the mongoDB collection
+      res.status(500).json({
+        error: `Error (\'/api/videos/\' POST route): ${err.message}`,
+        stack: err.stack,
+      });
+    }
+  } else {
+    // video is missing necessary (title, url, viewCount, publishedAt) fields; throw error
+    res.status(400).json({
+      error: `Error (\'/api/videos/\' POST route): At least 1 of the mandatory fields (\'title\', \'url\', \'viewCount\', & \'publishedAt\') are NOT in the request.`,
     });
   }
 });
