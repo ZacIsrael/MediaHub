6e19e53be733f3c4cd68609823c82ae41c42731e
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-07-16 16:25:34 -0400

    Implemented routes that retreive ALL social posts and ALL videos.

--- backend/routes/social-posts.js
+++ backend/routes/social-posts.js
@@ -14,29 +14,60 @@ const router = express.Router();
 
 // route that stores a social post in the mongoDB collection
 router.post("/", async (req, res) => {
-    // Thoroughly check the body of the request for errors (will create a 
-    // an auxillary function in helpers.js later on)
+  // Thoroughly check the body of the request for errors (will create a
+  // an auxillary function in helpers.js later on)
 
-    // happy path (for now)
-    // extract the necessary fields from the body of the request
-    const { platform, url, caption, hashtags, engagement } = req.body;
+  // happy path (for now)
+  // extract the necessary fields from the body of the request
+  const { platform, url, caption, hashtags, engagement } = req.body;
 
-    try {
-        // insert the new video into the mongoDB collection
-        const newSocialPost = await SocialPosts.create({ platform, url, caption, hashtags, engagement  });
+  try {
+    // insert the new video into the mongoDB collection
+    const newSocialPost = await SocialPosts.create({
+      platform,
+      url,
+      caption,
+      hashtags,
+      engagement,
+    });
 
-        res.status(201).json({
-            message:  `Successfully inserted social post into the \'social_posts\' mongoDB collection.`,
-            socialPost: newSocialPost
-        })
+    res.status(201).json({
+      message: `Successfully inserted social post into the \'social_posts\' mongoDB collection.`,
+      socialPost: newSocialPost,
+    });
+  } catch (err) {
+    // Error inserting the social post into the mongoDB collection
+    res.status(500).json({
+      error: `Error (\'/api/social-posts/\' POST route): ${err.message}`,
+      stack: err.stack,
+    });
+  }
+});
 
-    } catch(err) {
-        // Error inserting the social post into the mongoDB collection
-        res.status(500).json({
-            error: `Error (\'/api/social-posts/\' POST route): ${err.message}`,
-            stack: err.stack
-        });
+// route that retrieves all social posts from the 'socialposts' mongoDB collection
+router.get("/", async (req, res) => {
+  try {
+    // returns an array of all of the socialposts in the mongoDB collection
+    const allSocialPosts = await SocialPosts.find();
+
+    if (allSocialPosts.length === 0) {
+      res.status(200).json({
+        message: `The \'socialposts\' mongoDB collection is empty.`,
+        socialPosts: allSocialPosts,
+      });
+    } else {
+      res.status(200).json({
+        message: `Successfully retreived all social posts from the \'socialposts\' mongoDB collection.`,
+        socialPosts: allSocialPosts,
+      });
     }
+  } catch (err) {
+    // Error retreiving all social posts from the mongoDB collection
+    res.status(500).json({
+      error: `Error (\'/api/social-posts/\' GET route): ${err.message}`,
+      stack: err.stack,
+    });
+  }
 });
 
 export default router;
--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -14,29 +14,60 @@ const router = express.Router();
 
 // route that stores a video in the mongoDB collection
 router.post("/", async (req, res) => {
-    // Thoroughly check the body of the request for errors (will create a 
-    // an auxillary function in helpers.js later on)
+  // Thoroughly check the body of the request for errors (will create a
+  // an auxillary function in helpers.js later on)
 
-    // happy path (for now)
-    // extract the necessary fields from the body of the request
-    const { title, url, tags, viewCount, publishedAt } = req.body;
+  // happy path (for now)
+  // extract the necessary fields from the body of the request
+  const { title, url, tags, viewCount, publishedAt } = req.body;
 
-    try {
-        // insert the new video into the mongoDB collection
-        const newVideo = await Videos.create({ title, url, tags, viewCount, publishedAt });
+  try {
+    // insert the new video into the mongoDB collection
+    const newVideo = await Videos.create({
+      title,
+      url,
+      tags,
+      viewCount,
+      publishedAt,
+    });
 
-        res.status(201).json({
-            message:  `Successfully inserted video into the \'videos\' mongoDB collection.`,
-            video: newVideo
-        })
+    res.status(201).json({
+      message: `Successfully inserted video into the \'videos\' mongoDB collection.`,
+      video: newVideo,
+    });
+  } catch (err) {
+    // Error inserting the video into the mongoDB collection
+    res.status(500).json({
+      error: `Error (\'/api/videos/\' POST route): ${err.message}`,
+      stack: err.stack,
+    });
+  }
+});
 
-    } catch(err) {
-        // Error inserting the video into the mongoDB collection
-        res.status(500).json({
-            error: `Error (\'/api/videos/\' POST route): ${err.message}`,
-            stack: err.stack
-        });
+// route that retrieves all videos from the 'videos' mongoDB collection
+router.get("/", async (req, res) => {
+  try {
+    // returns an array of all of the videos in the mongoDB collection
+    const allVideos = await Videos.find();
+
+    if (allVideos.length === 0) {
+      res.status(200).json({
+        message: `There are no videos in the \'videos\' mongoDB collection.`,
+        videos: allVideos,
+      });
+    } else {
+      res.status(200).json({
+        message: `Successfully retreived all videos from the \'videos\' mongoDB collection.`,
+        videos: allVideos,
+      });
     }
+  } catch (err) {
+    // Error retreiving all videos from the mongoDB collection
+    res.status(500).json({
+      error: `Error (\'/api/videos/\' GET route): ${err.message}`,
+      stack: err.stack,
+    });
+  }
 });
 
 export default router;
