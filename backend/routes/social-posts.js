b9ba0a5f1ccae31e5dc14f11b71259d452daa335
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-07-16 09:33:46 -0400

    Implemented happy path for adding a social post.

--- backend/routes/social-posts.js
+++ backend/routes/social-posts.js
@@ -8,6 +8,35 @@ import bodyParser from "body-parser";
 // Enables Cross-Origin Resource Sharing, allowing frontend apps on different ports (like React) to make requests to this backend
 import cors from "cors";
 
+import { SocialPosts } from "../models/social-posts.js";
+
 const router = express.Router();
+
+// route that stores a social post in the mongoDB collection
+router.post("/", async (req, res) => {
+    // Thoroughly check the body of the request for errors (will create a 
+    // an auxillary function in helpers.js later on)
+
+    // happy path (for now)
+    // extract the necessary fields from the body of the request
+    const { platform, url, caption, hashtags, engagement } = req.body;
+
+    try {
+        // insert the new video into the mongoDB collection
+        const newSocialPost = await SocialPosts.create({ platform, url, caption, hashtags, engagement  });
+
+        res.status(201).json({
+            message:  `Successfully inserted social post into the \'social_posts\' mongoDB collection.`,
+            socialPost: newSocialPost
+        })
+
+    } catch(err) {
+        // Error inserting the social post into the mongoDB collection
+        res.status(500).json({
+            error: `Error (\'/api/social-posts/\' POST route): ${err.message}`,
+            stack: err.stack
+        });
+    }
+});
 
 export default router;
