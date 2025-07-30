80c61a7cb92b74f7c8b7a0eb2ab0c8a1adf68eb8
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-07-30 09:49:14 -0400

    Implemented jest test for the getAllVideos() service method.

--- backend/tests/video.service.test.ts
+++ backend/tests/video.service.test.ts
@@ -1,9 +1,11 @@
 import { ObjectId } from "mongodb";
 import { videoService } from "../services/video.service";
 import { CreateVideoDTO } from "../dtos/video.dto";
+import { Videos } from "../models/video.model";
 
 // Tell jest to mock the Videos model itself so these tests aren't actually
 // connecting to the real MongoDB database
+/*
 jest.mock("../models/video.model", () => {
   return {
     Videos: jest.fn().mockImplementation((doc: any) => ({
@@ -19,6 +21,41 @@ jest.mock("../models/video.model", () => {
         __v: 0,
       }),
     })),
+  };
+});
+*/
+
+jest.mock("../models/video.model", () => {
+  return {
+    Videos: Object.assign(
+      jest.fn().mockImplementation((doc: any) => ({
+        ...doc,
+        // mock the save function (save() adds a document to a MongoDB collection)
+        save: jest.fn().mockResolvedValue({
+          _id: new ObjectId().toString(),
+          title: doc.title,
+          url: doc.url,
+          tags: doc.tags,
+          viewCount: doc.viewCount,
+          publishedAt: doc.publishedAt,
+          __v: 0,
+        }),
+      })),
+      {
+        // mock the find function  (find() retreives all of the documents from a MongoDB collection)
+        find: jest.fn().mockResolvedValue([
+          {
+            _id: "6883e33f837c683090b9c861",
+            title: "Rapper Interview 0",
+            url: "https://www.youtube.com",
+            tags: ["DMV", "Rap", "Hip-Hop", "Producer"],
+            viewCount: 20255,
+            publishedAt: "2024-03-19T14:00:00.000Z",
+            __v: 0,
+          },
+        ]),
+      }
+    ),
   };
 });
 
@@ -63,6 +100,51 @@ describe("videoService", () => {
         _id: expect.any(String),
         __v: 0,
       });
+    });
+  });
+
+  // testing getAllVideos() function
+  describe("getAllVideos", () => {
+    it("should return all videos", async () => {
+      const mockVideos = [
+        {
+          _id: "6883e33f837c683090b9c861",
+          title: "Rapper Interview 0",
+          url: "https://www.youtube.com",
+          tags: ["DMV", "Rap", "Hip-Hop", "Producer"],
+          viewCount: 20255,
+          publishedAt: "2024-03-19T14:00:00.000Z",
+          __v: 0,
+        },
+        {
+          _id: "6883e3b0837c683090b9c864",
+          title: "Rapper Interview 1",
+          url: "https://www.youtube.com",
+          tags: ["DMV", "Rap", "Hip-Hop"],
+          viewCount: 77272,
+          publishedAt: "2024-02-06T14:00:00.000Z",
+          __v: 0,
+        },
+        {
+          _id: "6883e3e4837c683090b9c866",
+          title: "Rapper Interview 2",
+          url: "https://www.youtube.com",
+          tags: ["DMV", "Rap", "Hip-Hop"],
+          viewCount: 22410,
+          publishedAt: "2024-04-17T14:00:00.000Z",
+          __v: 0,
+        },
+      ];
+
+      // Ensure the mocked find() returns all of mockVideos
+      (Videos.find as jest.Mock).mockResolvedValue(mockVideos);
+
+      // call the getAllVideos() function so that it can be tested
+      const result = await service.getAllVideos();
+      // console.log("result = ", result, "\nmockVideos = ", mockVideos);
+
+      // result from getAllVideos() should have the same structure as the mockVideos array
+      expect(result).toEqual(mockVideos);
     });
   });
 });
